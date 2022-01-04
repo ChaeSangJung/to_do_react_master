bc7d2d64e62a745a8d3882fee7062ff254045e50
Chae Sang, Jung <shtingstar@hanmail.net>
2022-01-04 22:59:30 +0900

    5.13 Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,32 @@
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { toDoSate } from "../atom";
 import { IToDo } from "../types";
 
-function ToDo({text}:IToDo){
+function ToDo({text, category, id}:IToDo){
+    const setTodos = useSetRecoilState(toDoSate);
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+        const {
+            currentTarget : {dataset : {name}},
+        } = event;
+        console.log(name);
+    }
+    console.log(setTodos)
     return (
         <li>
-            <span>{text}</span>
-            <button>Doing</button>
+            <span>{text}{category}</span>
+            {category !== "DOING" && (
+                <button data-name="DOING" onClick={onClick}>Doing</button>
+            )}
+            {category !=="TO_DO" && (
+                <button data-name="TO_DO" onClick={onClick}>TO DO</button>
+            )}
+            {category !=="DONE" && (
+                <button data-name="DONE" onClick={onClick}>DONE</button>
+            )}
+            {/* <button>Doing</button>
             <button>To Do</button>
-            <button>Done</button>
+            <button>Done</button> */}
         </li>
     );
 }
